1633ae087346987e5acdb340b6450da19660af90
toothlesswonder <45980275+toothlesswonder@users.noreply.github.com>
2018-12-25 22:42:29 -0500

    Solved day 4 part 1

--- day4/day4_part1.py
+++ day4/day4_part1.py
@@ -1,91 +1,113 @@
-''' NOTE: this would be so simple with pandas but don't have it on pythonista! '''
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 24 20:17:26 2018
+@author: toothlesswonder
+"""
 
-class Event:
-	
-	# attributes
-	def __init__(self, time, status):
-		self.time = time
-		self.status = status
-		self.guard = ''
-		self.duration = ''
-	
-		# print string	
-	def __str__(self):
-		return "%s %s" % (self.datetime, self.event)
-
-
-''' parse input and create events '''
-from datetime import datetime
-
-event_log = []
-
-with open("puzzle_input.txt", "r") as f:
-	for item in f:
-		time = datetime.strptime(item.split('] ')[0], '[%Y-%m-%d %H:%M')
-		status = item.split('] ')[1]
-		event = Event(time,status)
-		event_log.append(event)
-
-
-''' sort event log on datetime '''
-	
-event_log.sort(key=lambda r: r.time)
-
-
-for e in event_log:
-	if e.status[0] == 'G':
-		guard = e.status.split()[1][1:]
-		e.guard = guard
-		e.status = "start"
-	elif e.status[0] == 'f':
-		e.status = "asleep"
-		e.guard = guard
-	elif e.status[0] == 'w':
-		e.status = "awake"
-		p.duration = e.time - p.time
-	else:
-		pass
-	# remember previous for next loop
-	p = e
-		
-for e in event_log[:25]:
-	if e.status == "asleep":
-		print(e.time)
-		print(e.status)
-		print(e.duration)
-		print(e.guard)
-		print('. . .')
-
-
-''' now that i have durations and guards, i only need to care about the "asleep" events '''	
-
-sleep = [e for e in event_log if e.status == "asleep"]
-
-''' which guard has most minutes asleep? '''
-#this would be one line of code in pandas! 
-
-guards = []
-for e in sleep:
-	guards.append(e.guard)
-	
-guards = list(set(guards))
-
-print(guards)
+# imports
+import pandas as pd
+import numpy as np
 
 
 
+''' INGEST '''
 
-guard_sums = {}
-for g in guards:
-	guard_sums[g] = 0
-	
-for e in sleep:
-	guard_sums[e.guard] = 
-
-
-	
+# create dataframe with event data
+df = pd.read_csv('advent/puzzle_input.txt', sep='] ', engine="python", header=None)
+df.columns = ['date', 'event']
 
 
 
+''' CLEAN '''
+
+'''
+Note: parsing the date gets an out of bounds datetime error! 32 bit integer 
+can't handle year 1518.  I should come back and learn to do a period index:
+http://pandas-docs.github.io/pandas-docs-travis/timeseries.html#representing-out-of-bounds-spans
+
+But for now I am going to be lazy.
+
+'''
+
+# being lazy
+df['date'] = [row.replace("[1518","2018") for row in df.date]
+
+# parse times 
+df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d %H:%M')
+
+# code to set datetime as index and then sort
+df = df.set_index('date').sort_index()
+
+#this is just to make my data look cleaner; not really needed 
+def parse_status(event_string):
+    if event_string[0] == 'G':
+        status = 'start'
+    elif event_string[0] == 'f':
+        status= "asleep"
+    elif event_string[0] == 'w':
+        status = "awake"
+    else:
+        print('unexpected status:', event_string)
+    return status
+
+# create clean status
+df['status'] = [parse_status(row) for row in df.event]
+
+# get numeric guard ID as a column; fill non-guard columns with NaN (for now)
+df['guard'] = [row.split()[1][1:] if "Guard" in row else np.NaN for row in df.event]
+
+# fill guard ID for following events (that have NaN)
+df = df.fillna(method='ffill')
+
+# calculate durations
+# note: calculating for all rows, but these only make sense for "asleep" rows
+df['shift'] = df.index
+df['shift'] = df['shift'].shift(-1)
+df['duration'] = df['shift'] - df.index
+
+# drop everything but the asleep rows (which now contain all needed info)
+df = df[df['status'] == 'asleep'][['status', 'guard', 'duration']]
+
+# mmm, so fresh and clean!
+df.head(25)
 
 
+
+''' STEP 1: find guard that is asleep most often '''
+
+# thank you pandas for doing this in *ONE LINE*  :)
+guard_sums = df[['guard', 'duration']].groupby(['guard']).sum().reset_index()
+
+# the guard with the most minutes is: 1993
+guard = guard_sums.sort_values('duration', ascending=False).iloc[0]['guard']
+print('Guard #%s has the most minutes' % (guard))
+
+
+      
+''' STEP 2: find the minute of the day when Guard 1993 is asleep most often '''
+
+# create new df with minutes of the day (1440 rows)
+df2 = pd.DataFrame(pd.date_range('2018-01-01', '2018-01-02', freq="1min")[:-1].time)
+df2.columns = ['time']
+df2 = df2.set_index('time')
+
+# create a column for counts
+df2['count'] = 0
+
+# iterate through the 24 sleep sessions of guard 1993
+for index, row in df[df['guard'] == guard].iterrows():
+    datelist = pd.date_range(index, index + row['duration'], freq="1min").time
+    df3 = pd.DataFrame(datelist)
+    df3.columns = ['time']
+    for row in df3['time']:
+        df2.at[row, 'count'] = df2.loc[row]['count'] + 1
+
+max_count = df2['count'].max()
+
+print('There are %s possible answers:' % (len(df2.index[df2['count'] == max_count])))
+
+for item in df2.index[df2['count'] == max_count]:
+    print(int(guard)*item.minute)
+
+
+''' Note: the second possible answer 73741 was right. Why am I getting two? '''
